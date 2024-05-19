2b708169bec14ed55077370739116589c1fb7293
jjwon2149 <jjwon2149@gmail.com>
2024-05-19 18:03:26 +0900

    refactor: update async await and rename variables,functions

--- WordQuizDaily/WordQuizDaily/Views/Quiz/QuizViewModel.swift
+++ WordQuizDaily/WordQuizDaily/Views/Quiz/QuizViewModel.swift
@@ -8,99 +8,105 @@
 import Foundation
 import Kingfisher
 
+@MainActor
 class QuizViewModel: ObservableObject, NaverNetworkDelegate {
     
     let hardKoreanWords = HardKoreanWords()
     let wordNetwork = WordNetwork()
     
-    var choiceWord = [String]()
-    var correctWord: String = ""
-    var correctWordDefinition: String = ""
-    var wordDataDictionary = [String: WordData]()
-    let naverNetwork = NaverNetwork.shared
-    
+    @Published var choiceWord = [String]()
+    @Published var correctWord: String = ""
+    @Published var correctWordDefinition: String = ""
+    @Published var wordDataDictionary = [String: WordData]()
     @Published var imageData: NaverImageData?
     @Published var isLoading = false
+    @Published var errorMessage: String? //에러메세지
+    
+    var naverNetwork = NaverNetwork.shared
     
     init() {
-        correctWord = hardKoreanWords.hardWords.randomElement() ?? ""
-        generateChoices()
-        
         naverNetwork.delegate = self
-    }
-    
-    //보기 생성
-    func generateChoices() {
-        choiceWord.append(correctWord)
-        
-        while choiceWord.count < 4 {
-            let randomWord = hardKoreanWords.hardWords.randomElement()!
-            if !choiceWord.contains(randomWord) {
-                choiceWord.append(randomWord)
-            }
-        }
-        //보기 섞기
-        choiceWord.shuffle()
-        
-        //단어 검색 및 데이터 저장
-        for word in choiceWord {
-            wordNetwork.searchWord(word) { wordData in
-                self.handleWordData(word: word, wordData: wordData)
-            }
-        }
-        print(choiceWord)
-        isLoading = true // 네트워크 요청 시작 시 isLoading을 true
+        fetchData()
     }
     
     func fetchData() {
-        
-            correctWord = hardKoreanWords.hardWords.randomElement() ?? ""
-            
-            choiceWord.removeAll()
-            generateChoices()
-            
-            fetchCorrectWordDefinition()
-            fetchImageForWord(correctWord)
+        Task {
+            await setupNewQuiz()
         }
+    }
     
-    
-    //MARK: - KoreanWordSearchAPI
-    
-    // 단어 데이터 처리 메서드
-    func handleWordData(word: String, wordData: WordData?) {
-        if let wordData = wordData {
-            wordDataDictionary[word] = wordData
-            // 정답 단어의 설명을 가져오기
-            if word == correctWord {
-                fetchCorrectWordDefinition()
-                fetchImageForWord(correctWord)
-            }
-        } else {
-            print("\(word) 단어 데이터를 가져오지 못함 ")
-            isLoading = false // 데이터를 가져오지 못한 경우 isLoading을 false로 설정하여 프로그레스 뷰를 숨김
+    //퀴즈 셋업
+    func setupNewQuiz() async {
+        isLoading = true
+        correctWord = hardKoreanWords.hardWords.randomElement() ?? ""
+        choiceWord = await generateChoices()
+        
+        do {
+            let wordData = try await wordNetwork.asyncSearchWord(correctWord)
+            await handleWordData(word: correctWord, wordData: wordData)
+        } catch {
+            handleNetworkError(error)
         }
         
         isLoading = false
     }
     
+    //보기 생성
+    func generateChoices() async -> [String] {
+        var choices = [correctWord]
+        while choices.count < 4 {
+            let randomWord = hardKoreanWords.hardWords.randomElement()!
+            if !choices.contains(randomWord) {
+                choices.append(randomWord)
+            }
+        }
+        return choices.shuffled()
+    }
+    
+    //MARK: - KoreanWordSearchAPI
+    
+    // 단어 데이터 처리 메서드
+    func handleWordData(word: String, wordData: WordData?) async {
+        DispatchQueue.main.async {
+            if let wordData = wordData {
+                self.wordDataDictionary[word] = wordData
+                if word == self.correctWord {
+                    Task {
+                        await self.fetchCorrectWordDefinition()
+                        await self.fetchImageForWord(self.correctWord)
+                    }
+                }
+            } else {
+                self.errorMessage = "\(word) 단어 데이터를 가져오지 못함"
+            }
+            self.isLoading = false
+        }
+    }
+    
     // 정답 단어의 설명 가져오기
-    func fetchCorrectWordDefinition() {
-        isLoading = true
-        
+    func fetchCorrectWordDefinition() async {
         guard let wordData = wordDataDictionary[correctWord] else {
-            correctWordDefinition = "설명을 가져올 수 없습니다."
-            isLoading = false // 작업 완료
+            DispatchQueue.main.async {
+                self.correctWordDefinition = "설명을 가져올 수 없습니다."
+                self.isLoading = false
+            }
             return
         }
         
         if let firstSense = wordData.channel.item.first?.sense.first {
-            correctWordDefinition = firstSense.definition
-            print(correctWordDefinition)
+            DispatchQueue.main.async {
+                self.correctWordDefinition = firstSense.definition
+                print(self.correctWordDefinition)
+            }
         } else {
-            correctWordDefinition = "설명을 찾을 수 없습니다."
+            DispatchQueue.main.async {
+                self.correctWordDefinition = "설명을 찾을 수 없습니다."
+            }
+        }
+        DispatchQueue.main.async {
+            self.isLoading = false
         }
         
-        isLoading = false // 작업 완료
     }
     
     //정답 확인 메서드 확인용
@@ -109,12 +115,16 @@ class QuizViewModel: ObservableObject, NaverNetworkDelegate {
     }
     //MARK: - NaverSearchAPI
     
-    func fetchImageForWord(_ word: String) {
-        isLoading = true
+    func fetchImageForWord (_ word: String) {
+        DispatchQueue.main.async {
+            self.isLoading = true
+        }
         
         naverNetwork.requestSearchImage(query: word){ [weak self] in
             // 이미지 데이터 로드 완료 시에만 isLoading을 false로 설정
-            self?.isLoading = false
+            DispatchQueue.main.async {
+                self?.isLoading = false
+            }
         }
     }
     
@@ -122,8 +132,15 @@ class QuizViewModel: ObservableObject, NaverNetworkDelegate {
     func imageDataUpdated(_ imageData: NaverImageData?) {
         //publishing changes from background threads is not allowed의 에러가 나와 변경.
         DispatchQueue.main.async { [weak self] in
-            guard let self = self else { return }
-            self.imageData = imageData
+            self?.imageData = imageData
+        }
+    }
+    
+    //MARK: - Error Handling
+    func handleNetworkError(_ error: Error) {
+        DispatchQueue.main.async {
+            self.errorMessage = error.localizedDescription
+            self.isLoading = false
         }
     }
 }
