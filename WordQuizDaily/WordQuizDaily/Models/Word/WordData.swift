450db412c446ee45142433f792177cf4029d8cdc
jjwon2149 <jjwon2149@gmail.com>
2024-05-24 16:46:08 +0900

    refactor: remove comment

--- WordQuizDaily/WordQuizDaily/Models/Word/WordData.swift
+++ WordQuizDaily/WordQuizDaily/Models/Word/WordData.swift
@@ -26,21 +26,3 @@ struct Sense: Codable {
     let link: String
     let type: String
 }
-
-
-
-
-
-//"channel": {
-//    "item": [
-//        {
-//            "word": "나무",
-//            "sense": {
-//                "target_code": 368281,
-//                "sense_no": 1,
-//                "definition": " 줄기나 가지가 목질로 된 여러해살이 식물.",
-//                "pos": "명사",
-//                "link": "http://opendict.korean.go.kr/dictionary/view?sense_no=368281",
-//                "type": "일반어"
-//            }
-//        }
