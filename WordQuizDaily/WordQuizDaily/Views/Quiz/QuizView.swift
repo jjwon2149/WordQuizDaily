8b30e11908565862fc6bb2cff3a5aac8fc11ead4
jjwon2149 <jjwon2149@gmail.com>
2024-05-26 21:30:43 +0900

    refactor: QuizView 나누기

--- WordQuizDaily/WordQuizDaily/Views/Quiz/QuizView.swift
+++ WordQuizDaily/WordQuizDaily/Views/Quiz/QuizView.swift
@@ -13,61 +13,103 @@ struct QuizView: View {
     @State var isAnswerCorrect = false
     
     var body: some View {
-        
         VStack{
-            
-            //MARK: - 문제의 이미지
+            //문제 이미지 뷰
             VStack{
-                if quizViewModel.isLoading {
-                    ProgressView("Loading...")
-                        .frame(width: 200, height: 200)
-                } else {
-                    if let imageData = quizViewModel.imageData {
-                        if imageData.items.count > 2 {
-                            let thirdImage = imageData.items[2]
-                            KFImage(URL(string: thirdImage.link))
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 200, height: 200)
-                        } else if let firstImage = imageData.items.first {
-                            KFImage(URL(string: firstImage.link))
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 200, height: 200)
-                        }
-                    }
-                }
+                answerImageView(quizViewModel: quizViewModel)
             }
             .padding()
-            
             Spacer()
-            
-            //MARK: - 문제의 뜻
+            //문제 설명 뷰
             VStack(alignment: .leading){
-                ScrollView {
-                    Text(quizViewModel.correctWordDefinition)
-                        .fixedSize(horizontal: false, vertical: true) // 가로 스크롤 비활성화, 세로 스크롤 활성화
-                }
-                .padding()
-                .frame(width: UIScreen.main.bounds.width - 20, height: 100)
-                .foregroundColor(.white)
-                .background(Color.black)
-                .cornerRadius(8)
-                .shadow(color: .gray, radius: 2, x: 0, y: 2)
-                
+                answerExplainView(quizViewModel: quizViewModel)
             }
-            
             Spacer()
-            
-            //MARK: - 보기
+            //문항
             VStack(alignment: .leading){
-                
                 ChoiceView(quizViewModel: quizViewModel)
             }
-            
-            
         }//VStack
         .padding()
+    }
+}
+
+//MARK: - 문제 이미지 뷰
+struct answerImageView: View {
+    @ObservedObject var quizViewModel: QuizViewModel
+    
+    var body: some View {
+        if quizViewModel.isLoading {
+            ProgressView("Loading...")
+                .frame(width: 200, height: 200)
+        } else {
+            if let imageData = quizViewModel.imageData {
+                if imageData.items.count > 2 {
+                    let thirdImage = imageData.items[2]
+                    KFImage(URL(string: thirdImage.link))
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200)
+                } else if let firstImage = imageData.items.first {
+                    KFImage(URL(string: firstImage.link))
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200)
+                }
+            }
+        }
+    }
+}
+
+//MARK: - 문제 설명 뷰
+struct answerExplainView: View {
+    
+    @ObservedObject var quizViewModel: QuizViewModel
+    var body: some View {
+        ScrollView {
+            Text(quizViewModel.correctWordDefinition)
+                .fixedSize(horizontal: false, vertical: true) // 가로 스크롤 비활성화, 세로 스크롤 활성화
+        }
+        .padding()
+        .frame(width: UIScreen.main.bounds.width - 20, height: 100)
+        .foregroundColor(.white)
+        .background(Color.black)
+        .cornerRadius(8)
+        .shadow(color: .gray, radius: 2, x: 0, y: 2)
+    }
+}
+
+
+//MARK: - 문항 뷰
+struct ChoiceView: View {
+    
+    @ObservedObject var quizViewModel: QuizViewModel
+    @State private var isAnswerCorrect = false
+    
+    var body: some View {
+        
+        VStack{
+            ForEach(quizViewModel.choiceWord.indices, id: \.self) { index in
+                let word = quizViewModel.choiceWord[index]
+                Button(action: {
+                    isAnswerCorrect = quizViewModel.checkAnswer(selectedWord: word)
+                    if isAnswerCorrect {
+                        quizViewModel.fetchData()
+                    }
+                    print("선택된 단어: \(word), 정답?: \(isAnswerCorrect)")
+                }) {
+                    Text("  \(index + 1).  \(word) ")
+                        .padding()
+                        .frame(width: UIScreen.main.bounds.width - 20, height: 50, alignment: .leading)
+                        .foregroundColor(.white)
+                        .background(Color.black)
+                        .cornerRadius(8)
+                        .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                    
+                }
+                .padding(5)
+            }
+        }
     }
 }
 
